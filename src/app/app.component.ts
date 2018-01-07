d85a8f1d738a1b8321f59e732dea2ba7a878c808
evantoh <evanmwenda@gmail.com>
2018-01-07 14:40:13 +0300

    create a new property in class quotes and  give it a type of string array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent {
 
  constructor(){
 
-   this.quotes = ["If you would convince a man that he does wrong, do right. But do not care to convince him. Men will believe what they see. Let them see.",
-   "Hide yourself in God, so when a man wants to find you he will have to go there first",
+   this.quotes = ["If you would convince a man that he does wrong, do right. But do not care to convince him. Men will believe what they see.Let them see.",
+   "Hide yourself in God, so when a man wants to find you he will have to go there first.",
    "I would rather be an artist than a leader. Ironically, a leader has to follow the rules.",
  "To bring up a child in the way he should go, travel that way yourself once in a while.",
 "I may do some good before I am dead--be a sort of success as a frightful example of what not to do; and so illustrate a moral story.",
