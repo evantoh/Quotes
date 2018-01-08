7b61731602bef8fe13f3b6263da50f7658bfdf54
evantoh <evanmwenda@gmail.com>
2018-01-08 18:12:45 +0300

    add custom pipes to calculate how long the quote is created

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import{Quote} from '../quote'
 export class QuoteComponent implements OnInit {
   title = 'App Quotes';
   quotes = [
-    new Quote(1,'doing good and bad things to a man.','If you would convince a man that he does wrong, do right. But do not care to convince him. Men will believe what they see.Let them see.'),
-   new  Quote(2,'Hide yourself in GOd.','Hide yourself in God, so when a man wants to find you he will have to go there first.'),
-  new Quote(3,'comparing artist and leaders.','I would rather be an artist than a leader. Ironically, a leader has to follow the rules.'),
- new Quote(4,'bringing up a child.','To bring up a child in the way he should go, travel that way yourself once in a while.'),
-new Quote(5,'success before death.','I may do some good before I am dead--be a sort of success as a frightful example of what not to do; and so illustrate a moral story.'),
-new Quote(6,'matching your life blueprint.','It is not until you change your identity to match your life blueprint that you will understand why everything in the past never worked.'),
+    new Quote(1,'doing good and bad things to a man.','If you would convince a man that he does wrong, do right. But do not care to convince him. Men will believe what they see.Let them see.',new Date(2018,3,14)),
+   new  Quote(2,'Hide yourself in GOd.','Hide yourself in God, so when a man wants to find you he will have to go there first.,',new Date(2018,2,15)),
+  new Quote(3,'comparing artist and leaders.','I would rather be an artist than a leader. Ironically, a leader has to follow the rules.',new Date(2018,1,28)),
+ new Quote(4,'bringing up a child.','To bring up a child in the way he should go, travel that way yourself once in a while.',new Date(2018,6,13)),
+new Quote(5,'success before death.','I may do some good before I am dead--be a sort of success as a frightful example of what not to do; and so illustrate a moral story.',new Date(2018,3,13)),
+new Quote(6,'matching your life blueprint.','It is not until you change your identity to match your life blueprint that you will understand why everything in the past never worked.',new Date(2018,4,177)),
 ]
 completeQuote(isComplete,index){
         if (isComplete){
